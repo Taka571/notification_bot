d3c4ede3f7e0dd849df84e7b2ce06caf508b8312
Takahiro <tk.iyei@gmail.com>
2019-09-08 17:23:04 +0900

    heroku用の修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get "/callback"  => "notifybots#index"
+  # post "/callback" => "notifybots#callback"
+  post "/recieve" => "notify_bots#recieve"
 end
