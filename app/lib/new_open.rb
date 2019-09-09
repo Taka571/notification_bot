690a2d6c8ed00c27266b61b44a194073058b4a51
Takahiro <tk.iyei@gmail.com>
2019-09-10 01:14:09 +0900

    new openを通知するクラスを追加

---
+++ app/lib/new_open.rb
@@ -0,0 +1,55 @@
+class NewOpen
+  require 'nokogiri'
+  require 'open-uri'
+  require 'line/bot'
+
+  attr_reader :ids
+
+  def initialize
+    url = 'https://tabelog.com/tokyo/rstLst/cond16-00-00/ramen/'
+    charset = nil
+    html = open(url) do |u|
+      charset = u.charset
+      u.read
+    end
+    doc = Nokogiri::HTML.parse(html, nil, charset)
+    @ids = article_info(doc)
+  end
+
+  def notify
+    if ids.blank?
+      client.push_message(ENV['LINE_USER_ID'], {"type": "text", "text": "新着のオープンはありません"})
+      return
+    end
+    content = Restaurant.create_line_messages(ids)
+    response = client.push_message(ENV['LINE_USER_ID'], content)
+    raise unless response.code == "200"
+  rescue => e
+    client.push_message(ENV['LINE_USER_ID'], {"type": "text", "text": "エラー発生中"})
+  end
+
+  private
+
+  def article_info(doc)
+    res = doc.xpath("//*[@id='column-main']/ul/li")
+    restaurant_ids = res.map.with_index(1) do |r, i|
+      new_restaurant = Restaurant.new(
+        name: r.xpath("//li[#{i}]/div[2]/div[1]/div/div/div/a")&.text || "no name",
+        image: r.xpath("//li[#{i}]/div[2]/div[2]").css("img")&.first&.values&.slice(2) || "no image",
+        place: r.xpath("//li[#{i}]/div[2]/div[1]/div/div/div/span")&.text[/(.*m)/] || "not found",
+        url: r.xpath("//li[#{i}]/div[2]/div[1]/div/div/div/a")&.first&.values&.slice(3) || "not found",
+        open_date: r.xpath("//li[#{i}]/div[2]/div[2]/div[1]/div[2]/p").text.gsub("年", "/").to_date
+      )
+      next if Restaurant.find_by(name: new_restaurant.name) || new_restaurant.name == "no name" || new_restaurant.name == ""
+      new_restaurant.save!
+      new_restaurant.id
+    end.compact.take(10)
+  end
+
+  def client
+    @client ||= Line::Bot::Client.new do |config|
+      config.channel_secret = ENV['LINE_CHANNEL_SECRET']
+      config.channel_token = ENV['LINE_CHANNEL_TOKEN']
+    end
+  end
+end
