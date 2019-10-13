a87072e9eb492ab9af282b366325903c80bc95c1
Takahiro <tk.iyei@gmail.com>
2019-10-13 15:36:22 +0900

    メーセージで入力された店を検索するルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
-  # get "/callback"  => "notifybots#index"
-  # post "/callback" => "notifybots#callback"
-  post "/recieve" => "notify_bots#recieve"
+  post "/restaurants" => "restaurants#show"
 end
