776001ceddb7b7a3eac704def8fa879894f539d5
Takahiro <tk.iyei@gmail.com>
2019-10-19 14:55:42 +0900

    api用のindexアクションを追加

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -1,6 +1,14 @@
 module Api
   module V1
     class RestaurantsController < ApplicationController
+      def index
+        restaurants = Restaurant.all.order(:open_date)
+        if restaurants.present?
+          render json: { status: "success", messages: "Loaded all restaurant", data: restaurants }
+        else
+          render json: { status: "failure", messages: "No records" }
+        end
+      end
     end
   end
 end
