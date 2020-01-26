649b7d265251a1ad3c8ac9449b13f35d3cce3778
Takahiro <tk.iyei@gmail.com>
2020-01-26 18:00:17 +0900

    地球の歩き方特派員ブログを追加

---
+++ app/lib/line_messages/how_to_walk_earth_traveler_blog.rb
@@ -0,0 +1,100 @@
+module LineMessages::HowToWalkEarthTravelerBlog
+  def notify
+    line_messages = create_line_messages
+    raise if line_messages.blank?
+    response = client.push_message(ENV['LINE_USER_ID'], line_messages)
+    raise unless response.code == "200"
+  rescue => e
+    error_client.push_message(
+      ENV['LINE_USER_ID'],{
+        "type": "text",
+        "text": "Error has occurred in #{self.class}, reason: #{response.body&.force_encoding("UTF-8")}"
+      }
+    )
+  end
+
+  private
+
+  def client
+    @client ||= Line::Bot::Client.new do |config|
+      config.channel_secret = ENV['LINE_HTWETB_CHANNEL_SECRET']
+      config.channel_token = ENV['LINE_HTWETB_CHANNEL_TOKEN']
+    end
+  end
+
+  def create_line_messages
+    {
+      "type": "flex",
+      "altText": blogs.map {|blog| blog[:title]}.join(" / "),
+      "contents":
+      {
+        "type": "carousel",
+        "contents": line_messages,
+      }
+    }
+  end
+
+  def line_messages
+    blogs.map do |blog|
+      {
+        "type": "bubble",
+        "hero": {
+          "type": "image",
+          "size": "full",
+          "aspectRatio": "20:13",
+          "aspectMode": "cover",
+          "url": blog[:image],
+          "action": {
+            "type": "uri",
+            "label": "画像",
+            "uri": blog[:page_url],
+          }
+        },
+        "body":
+        {
+          "type": "box",
+          "layout": "vertical",
+          "spacing": "sm",
+          "contents": [
+            {
+              "type": "text",
+              "text": blog[:title],
+              "wrap": true,
+              "margin": "xl",
+              "flex": 0,
+              "weight": "bold",
+            },
+            {
+              "type": "text",
+              "text": blog[:content],
+              "wrap": true,
+              "size": "md",
+              "action": {
+                "type": "uri",
+                "label": blog[:title],
+                "uri": blog[:page_url],
+              }
+            },
+          ]
+        },
+        "footer": {
+          "type": "box",
+          "layout": "vertical",
+          "spacing": "sm",
+          "backgroundColor": "#00a5de",
+          "contents": [
+            {
+              "type": "button",
+              "style": "link",
+              "action": {
+                "type": "uri",
+                "label": "全て見る",
+                "uri": blog[:page_url]
+              }
+            }
+          ]
+        }
+      }
+    end
+  end
+end
---
+++ app/lib/scraping/how_to_walk_earth_traveler_blog.rb
@@ -0,0 +1,27 @@
+class Scraping::HowToWalkEarthTravelerBlog < Scraping::Base
+  include LineMessages::HowToWalkEarthTravelerBlog
+  attr_reader :blogs
+  # 地球の歩き方特派員ブログ
+  PAGE_URL = "https://tokuhain.arukikata.co.jp".freeze
+
+  def initialize
+    driver = Selenium::WebDriver.for(:chrome)
+    driver.navigate.to(PAGE_URL)
+    @blogs = []
+    5.upto(14) do |num|
+      @blogs << {
+        title:       driver.find_element(:xpath, "//*[@id='main_column']/div[#{num}]/dt/a/img").attribute("alt"),
+        content:     driver.find_element(:xpath, "//*[@id='main_column']/div[#{num}]/dd").text,
+        page_url:    driver.find_element(:xpath, "//*[@id='main_column']/div[#{num}]/dd/a").attribute("href"),
+        image:       driver.find_element(:xpath, "//*[@id='main_column']/div[#{num}]/dt/a/img").attribute("src"),
+      }
+    end
+  rescue => e
+    error_client.push_message(
+      ENV['LINE_USER_ID'],{
+        "type": "text",
+        "text": "#{e.class} has occurred in #{self.class}"
+      }
+    )
+  end 
+end
